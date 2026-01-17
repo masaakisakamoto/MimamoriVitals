886b60c575674e58d31932c52217383994e41032
Masaaki Sakamoto <mas.sakamoto.biz@gmail.com>
2026-01-17 22:33:58 +0900

    Initial Commit

---
+++ MimamoriVitals/MimamoriVitalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimamoriVitalsApp.swift
+//  MimamoriVitals
+//
+//  Created by Masaaki Sakamoto on 2026/01/17.
+//
+
+import SwiftUI
+
+@main
+struct MimamoriVitalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
